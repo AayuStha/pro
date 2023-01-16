466d3429b076faa6f1dbce6519fed62ebbd0f5a9
Aayush Shrestha <95739774+AayuStha@users.noreply.github.com>
2023-01-16 22:38:37 +0545

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,54 @@
+// const names = ['Aayush','Sakshi','Rosy', 'Rasna'];
+
+// console.log(names[1]);
+
+// let email = 'a.ius.xtha@gmail.com';
+
+// // let result = email.lastIndexOf('u');
+
+// let result = email.replace('a', 's');
+
+// let result = email.slice('a', 's');
+
+// let age=21;
+// let year= 2002;
+
+//  let firstName = 'Aayush';
+// let secondName = 'Shrestha';
+
+// let email = 'abc@aayush.com'
+
+// // let result = fullName.lastIndexOf('r');
+
+// // console.log(fullName.toUpperCase());
+// // console.log(fullName.toLowerCase());
+
+// let index = email.indexOf('@');
+// console.log('index of the @ sign:', index);
+
+// let r = 5;
+// const pi = 3.14;
+
+// console.log(r, pi);
+
+// let heart = 20;
+
+// heart *= 30;
+
+// console.log(heart);
+
+// let result = 15 % 2;
+
+// // console.log(result);
+
+// const title = 'Best awards of 2022';
+// const author = 'Asit Kumar modi';
+// const likes = '400k';
+
+// // let result = title + ' goes to ' + author + ' with ' + likes +' likes.';
+// // console.log(result);
+
+// let result = `${title} goes to ${author} with ${likes} likes.`;
+// console.log(result);
+
+alert('This is a short and sweet messege only your boyfriend can do for you. I hope you liked it.😚💋Always Smile and shine my future wife, my soulmate.❤️This idiot loves you unconditionally.😍I hope this will make your night.❤️');
